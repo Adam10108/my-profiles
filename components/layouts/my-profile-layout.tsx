10a8b74cfc45314d5075b704c9dee0550d06221c
Adam10108 <adam_atlas1998@hotmail.com>
2021-08-16 20:14:20 +0700

    Content layout

--- components/layouts/my-profile-layout.tsx
+++ components/layouts/my-profile-layout.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
-import { AppBar } from '@/components/navbar'
-import { useStyles } from '@/utils/styled'
 
 export type MyProfileLayoutTypes = {
-  children: React.ReactNode
+  children: React.ReactChild | React.ReactChild[]
 }
 
 export const MyProfileLayout = ({ children }: MyProfileLayoutTypes) => {
-  const classes = useStyles()
-
-  return (
-    <Container maxWidth="xl">
-      <AppBar />
-      <div className={classes.spacer2} />
-      {children}
-    </Container>
-  )
+  return <Container maxWidth="xl">{children}</Container>
 }
--- pages/my-profile.tsx
+++ pages/my-profile.tsx
@@ -1,11 +1,25 @@
-import { Typography } from '@material-ui/core'
-
+import { Grid, Typography } from '@material-ui/core'
+import { AppBar } from '@/components/navbar'
 import { MyProfileLayout } from '@/components/layouts'
+import { useStyles } from '@/utils/styled'
 
 const MyProfile = () => {
+  const classes = useStyles()
+
   return (
     <MyProfileLayout>
-      <Typography variant="h1">My Profile Page</Typography>
+      <AppBar />
+
+      <div className={classes.spacer2} />
+
+      <Grid className={classes.px2} container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h4">Profile Card</Typography>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Typography variant="h4">Sub details</Typography>
+        </Grid>
+      </Grid>
     </MyProfileLayout>
   )
 }
